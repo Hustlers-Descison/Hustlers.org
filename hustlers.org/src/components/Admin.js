2565c7c44ff36485aae660bf5c6070ac208dd28d
BigThinkerYes <designaspnet4@hotmail.com>
2023-05-25 12:34:11 -0500

    Admin basic info

--- hustlers.org/src/components/Admin.js
+++ hustlers.org/src/components/Admin.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+import supabase from '../db/supabaseClient';
 // import styled from 'styled-components';
 
 export default function Admin(){
+    const navigate = useNavigate('');
+    const [userLog, setUserLog] = useState('');
+    const [userPwd, setuserPwd] = useState('');
+    const [formError, setFormError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!userLog || !userPwd){
+            setFormError('please fill out required fields');
+            return
+        }
+    // console.log(title, rating, method)
+    const { data, error} = await supabase
+        .from('admin')
+        .insert([{ userLog, userPwd}])
+        .select()
+    if (error){
+        console.log(error);
+        setFormError('please fill out required fields');
+    }
+    if (data){
+        console.log(data);
+        setFormError(null);
+        navigate('/');
+    }
+    }
+
     return(
      <>
      <div className='page admin'>
-        <h2>user dash</h2>
+     <form onSubmit={handleSubmit}>
+
+     </form>
+        <label htmlFor="password">UserName:</label>
+        <input
+        type=''
+            id=""
+            // value={}
+            onChange={(e) => setUserLog(e.target.value)}
+        />
+         <label htmlFor="password">Password:</label>
+        <input
+        type=''
+            id=""
+            // value={}
+            onChange={(e) => setuserPwd(e.target.value)}
+        />
+        <button>Login</button>
+        {formError && <p className="error">{formError}</p>}
      </div>
      </>
     )
