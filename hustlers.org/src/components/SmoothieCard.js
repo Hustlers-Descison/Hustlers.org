39937681c7423761435bae72715c2dd55ce658dd
BigThinkerYes <designaspnet4@hotmail.com>
2023-05-25 16:15:31 -0500

    added some style to SmoothieCard.js

--- hustlers.org/src/components/SmoothieCard.js
+++ hustlers.org/src/components/SmoothieCard.js
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import DeleteIcon from '@mui/icons-material/Delete';
-
 import  supabase  from "../db/supabaseClient";
+import styled from "styled-components";
+
+const BodyWrapper = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    margin: 20px;
+`
 export default function SmoothieCard ({ smoothie, onDelete }){
 
     const handleDelete = async () =>{
@@ -22,6 +28,7 @@ export default function SmoothieCard ({ smoothie, onDelete }){
     }
 
     return(
+        <BodyWrapper>
         <div className="smothie-card">
             <h3>{smoothie.title}</h3>
             <p>{smoothie.method}</p>
@@ -33,5 +40,6 @@ export default function SmoothieCard ({ smoothie, onDelete }){
                 <span onClick={handleDelete}><i>delete</i></span>
             </div>
         </div>
+        </BodyWrapper>
     )
 }
