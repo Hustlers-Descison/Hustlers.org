9a3dcb91d40953b72a94bc2ea3b83ef84841acf3
Igmer Castillo <icastillo@box.com>
2023-05-31 10:41:13 -0700

    Yarn Test

--- hustlers.org/src/components/Create.js
+++ hustlers.org/src/components/Create.js
@@ -1,74 +1,109 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import {useNavigate} from 'react-router-dom';
-import supabase  from '../db/supabaseClient';
+import { useNavigate } from 'react-router-dom';
+import supabase from '../db/supabaseClient';
 
 const BodyWrapper = styled.div`
-    display: flex;
-    gap: 0.5rem;
-    margin: 70px;
-`
-export default function Create(){
-    const navigate = useNavigate('');
-    const [title, setTitle] = useState('');
-    const [method, setMethod] = useState('');
-    const [rating, setRating] = useState('');
-    const [formError, setFormError] = useState(null);
+  display: flex;
+  gap: 0.5rem;
+  margin: 70px;
+`;
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!title || !method || !rating){
-            setFormError('please fill out required fields');
-            return
-        }
-    // console.log(title, rating, method)
-    const { data, error} = await supabase
-        .from('smoothies')
-        .insert([{ title, method, rating}])
-        .select()
-    if (error){
-        console.log(error);
-        setFormError('please fill out required fields');
+const ChatLog = styled.div`
+  /* Styles for chat log */
+`;
+
+const ChatBox = styled.div`
+  /* Styles for chat box */
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+`;
+
+const MessageInput = styled.textarea`
+  /* Styles for message input */
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+  border: none;
+  border-radius: 5px;
+`;
+
+const SendButton = styled.button`
+  /* Styles for send button */
+  padding: 0.5rem 1rem;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const ErrorText = styled.p`
+  /* Styles for error text */
+  color: red;
+`;
+
+export default function Create() {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState('');
+  const [formError, setFormError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) {
+      setFormError('Please enter a message.');
+      return;
     }
-    if (data){
+
+    try {
+      const user = supabase.auth.user();
+      if (!user) {
+        setFormError('You must be logged in to send a message.');
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from('messages')
+        .insert([{ userId: user.id, message }])
+        .select();
+
+      if (error) {
+        console.log(error);
+        setFormError('An error occurred while sending the message.');
+      } else {
         console.log(data);
         setFormError(null);
-        navigate('/home');
+        setMessage('');
+      }
+    } catch (error) {
+      console.log(error);
+      setFormError('An error occurred while sending the message.');
     }
+  };
 
-    }
-    return(
-     <>
-     <BodyWrapper>
-     <div className='page create'>
-        <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:</label>
-        <input
-        type='text'
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-        />
-        
-        <label htmlFor="method">Message:</label>
-        <textarea
-            id="method"
-            value={method}
-            onChange={(e) => setMethod(e.target.value)}
-        />
-
-        <label htmlFor="rating">Rate Message Urgency:</label>
-        <input
-            type='number'
-            id="rating"
-            value={rating}
-            onChange={(e) => setRating(e.target.value)}
-        />
-        <button>Create Message</button>
-        {formError && <p className="error">{formError}</p>}
-        </form>
-     </div>
-       </BodyWrapper>
-     </>
-    )
+  return (
+    <>
+      <nav>
+        {/* Navbar contents: Admin, Chat Log, Chat, Update */}
+      </nav>
+      <BodyWrapper>
+        <ChatLog>
+          {/* Display messages from multiple users */}
+        </ChatLog>
+        <ChatBox>
+          <form onSubmit={handleSubmit}>
+            <MessageInput
+              placeholder="Enter your message (up to 3000 words)"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <SendButton type="submit">Send</SendButton>
+            {formError && <ErrorText className="error">{formError}</ErrorText>}
+          </form>
+        </ChatBox>
+      </BodyWrapper>
+    </>
+  );
 }
